5133ee14556a04445264308388b800adb29dd3a2
Alex <amccallalexander@gmail.com>
2020-08-02 13:59:08 -0500

    Modified views/layouts and config files

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
   # validate the title and body fields
-  validates :title, presence: true, length: { minimum: 0, maximum: 100 }
-  validates :body, presence: true, length: { minimum: 0, maximum: 10000 }
+  validates :title, presence: true, length: {minimum: 0, maximum: 100}
+  validates :body, presence: true, length: {minimum: 0, maximum: 10000}
 end
